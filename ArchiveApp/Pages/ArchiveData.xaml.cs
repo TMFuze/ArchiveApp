350baf85c525246f4c6856457aad0ea8f5969041
FuzeMk <Salosterein@vk.com>
2023-06-23 16:35:34 +0300

    Version 1.01 (Добавлен поиск и возмножно работает удаление папки и данных привязанных к ней) !!!(Если не работает поменяй метод удаления папки)!!!

--- ArchiveApp/Pages/ArchiveData.xaml.cs
+++ ArchiveApp/Pages/ArchiveData.xaml.cs
@@ -4,17 +4,8 @@ using ArchiveApp.Windows;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ArchiveApp.Pages
 {
@@ -23,55 +14,67 @@ namespace ArchiveApp.Pages
     /// </summary>
     public partial class ArchiveData : Page
     {
+        private List<ArchiveFile> allItems;     
+
         public ArchiveData()
         {
             InitializeComponent();
-            
+
 
             FolderBox.SelectedValuePath = "Id";
             FolderBox.DisplayMemberPath = "Name";
             FolderBox.ItemsSource = DBCon.entObj.Folder.ToList();
             DGItems.ScrollIntoView(Name);
+
+            allItems = DBCon.entObj.ArchiveFile.ToList();
         }
+
+        
 
         private void FoldersBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            var selectedFolder = FolderBox.SelectedItem as Folder;
-            if (selectedFolder != null)
-            {
-                DGItems.ItemsSource = DBCon.entObj.ArchiveFile.Where(x => x.IdFolder == selectedFolder.Id).ToList();
-            }
+            
+                UpdateDataGrid();
+            
         }
 
         private void DelFolBtn_Click(object sender, RoutedEventArgs e)
         {
-           // var itemsForRemoving = DBCon.entObj.ArchiveFile.Where(x => x.IdFolder == FoldersBox.SelectedIndex).ToList();
-            //var productsForRemoving = FolderBox.Items.Cast<Folder>().ToList();
-            
-            //try
-            //{
+            var selectedFolder = FolderBox.SelectedItem as Folder;
+            if (selectedFolder != null)
+            {
+                var itemsForRemoving = DBCon.entObj.ArchiveFile.Where(x => x.IdFolder == selectedFolder.Id).ToList();
+                var productsForRemoving = new List<Folder> { selectedFolder };
 
-            //    var result = MessageBox.Show("Вы уверены?",
-            //       "Уведомление",
-            //       MessageBoxButton.YesNo,
-            //       MessageBoxImage.Question);
-            //    if (result == MessageBoxResult.Yes)
-            //    {
-            //        DBCon.entObj.Folder.RemoveRange(productsForRemoving);
-            //        //DBCon.entObj.ArchiveFile.RemoveRange(itemsForRemoving);
-            //        DBCon.entObj.SaveChanges();
-            //        MessageBox.Show("Данные удалены.");
-            //        DGItems.ItemsSource = DBCon.entObj.ArchiveFile.ToList();
-            //    }
-            //    else
-            //    {
-            //        DGItems.ItemsSource = DBCon.entObj.ArchiveFile.ToList();
-            //    }
-            //}
-            //catch (Exception ex)
-            //{
-            //    MessageBox.Show(ex.Message.ToString());
-            //}
+                try
+                {
+                    var result = MessageBox.Show("Вы уверены?",
+                        "Уведомление",
+                        MessageBoxButton.YesNo,
+                        MessageBoxImage.Question);
+
+                    if (result == MessageBoxResult.Yes)
+                    {
+                        DBCon.entObj.ArchiveFile.RemoveRange(itemsForRemoving);
+                        DBCon.entObj.Folder.RemoveRange(productsForRemoving);
+                        DBCon.entObj.SaveChanges();
+                        MessageBox.Show("Данные удалены.");
+
+                        // Обновление данных в DGItems и FoldersBox
+                        DGItems.ItemsSource = DBCon.entObj.ArchiveFile.ToList();
+                        FolderBox.ItemsSource = DBCon.entObj.Folder.ToList();
+                    }
+                    else
+                    {
+                        // Отмена удаления, обновление только DGItems
+                        DGItems.ItemsSource = DBCon.entObj.ArchiveFile.ToList();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
         }
 
         private void AddFolBtn_Click(object sender, RoutedEventArgs e)
@@ -124,6 +127,38 @@ namespace ArchiveApp.Pages
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        private void SearchBox_KeyUp_1(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == System.Windows.Input.Key.Enter)
+            {
+                UpdateDataGrid();
+            }
+        }
+
+        private void UpdateDataGrid()
+        {
+            var selectedFolder = FolderBox.SelectedItem as Folder;
+            string searchText = SearchBox.Text.ToLower();
+
+            // Получение коллекции всех элементов или элементов для выбранной папки
+            var items = (selectedFolder != null) ? allItems.Where(x => x.IdFolder == selectedFolder.Id) : allItems;
+
+            // Выполнение фильтрации поискового запроса
+            var filteredItems = items.Where(item =>
+                (item.Name != null && item.Name.ToLower().Contains(searchText)) ||
+                (item.Description != null && item.Description.ToLower().Contains(searchText))
+            );
+
+            // Обновление отображения в DataGrid
+            DGItems.ItemsSource = filteredItems;
+
+            // Выбор первого элемента, если есть результаты
+            if (filteredItems.Any())
+            {
+                DGItems.SelectedIndex = 0;
             }
         }
     }
